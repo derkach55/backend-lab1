e844d72e4e65e5d451f962ca03c322d71f6a3c70
Derkach Oleksandr <derkach03553@gmail.com>
2022-11-01 21:03:16 +0200

    get_records_by_user and get_records_by_user_and_category added

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import request, jsonify, abort
+from flask import request, abort
 from app import USERS, CATEGORIES, RECORD
 
 
@@ -36,16 +36,27 @@ def create_user():
 
 @app.post('/records/')
 def create_record():
-    try:
-        record = {
-            'id': RECORD[-1]['id'] + 1,
-            'user_id': request.get_json()['user_id'],
-            'category_id': request.get_json()['category_id'],
-            'payment': request.get_json()['payment']
-        }
-    except KeyError:
-        return abort(400)
-    RECORD.append(record)
-    return record
+    if request.method == 'POST':
+        try:
+            record = {
+                'id': RECORD[-1]['id'] + 1,
+                'user_id': request.get_json()['user_id'],
+                'category_id': request.get_json()['category_id'],
+                'payment': request.get_json()['payment']
+            }
+        except KeyError:
+            return abort(400)
+        RECORD.append(record)
+        return record
 
 
+@app.get('/records/<int:user_id>/')
+def get_records_by_user(user_id):
+    records = list(filter(lambda x: x['user_id'] == user_id, RECORD))
+    return records if records else abort(404)
+
+
+@app.get('/records/<int:user_id>/<int:category_id>/')
+def get_records_by_user_and_category(user_id, category_id):
+    records = list(filter(lambda x: x['user_id'] == user_id and x['category_id'] == category_id, RECORD))
+    return records if records else abort(404)
